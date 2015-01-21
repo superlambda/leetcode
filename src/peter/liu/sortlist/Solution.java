5fd68bdc7132772cfc1580717b5280e14af2ec27
superlambda <yingning200507@163.com>
2015-01-21 17:50:24 +0800

    sss

---
+++ src/peter/liu/sortlist/Solution.java
@@ -0,0 +1,27 @@
+package peter.liu.sortlist;
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ * 
+ * @author pcnsh197
+ *
+ */
+public class Solution {
+	public ListNode sortList(ListNode head) {
+
+	}
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
