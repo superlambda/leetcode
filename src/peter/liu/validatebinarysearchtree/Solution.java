a618f8a86aa381da3c3de0d2aadab333c54ee9a5
superlambda <yingning200507@163.com>
2015-09-05 11:30:25 +0800

    ac

---
+++ src/peter/liu/validatebinarysearchtree/Solution.java
@@ -0,0 +1,77 @@
+package peter.liu.validatebinarysearchtree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * The left subtree of a node contains only nodes with keys less than the node's
+ * key. The right subtree of a node contains only nodes with keys greater than
+ * the node's key. Both the left and right subtrees must also be binary search
+ * trees.
+ * 
+ * @author superlambda
+ *
+ */
+
+public class Solution {
+	private Map<TreeNode,Integer> maxMap=new HashMap<>();
+	private Map<TreeNode,Integer> minMap=new HashMap<>();
+	public boolean isValidBST(TreeNode root) {
+		if(root==null||(root.left==null&&root.right==null)){
+			return true;
+		}
+		boolean isValid=true;
+		
+		if(root.left!=null){
+			isValid = isValid &&isValidBST(root.left)&&root.val>getMaxLeft(root.left);
+		}
+		if(root.right!=null){
+			isValid = isValid &&isValidBST(root.right)&&root.val<getMinRight(root.right);
+		}
+		return isValid;
+	}
+	private int getMaxLeft(TreeNode root){
+		if(maxMap.containsKey(root)){
+			return maxMap.get(root);
+		}
+		int max=root.val;
+		if(root.left!=null){
+			max=Math.max(getMaxLeft(root.left), max);
+		}
+		if(root.right!=null){
+			max=Math.max(getMaxLeft(root.right), max);
+		}
+		maxMap.put(root, max);
+		return max;
+	}
+	
+	private int getMinRight(TreeNode root){
+		if(minMap.containsKey(root)){
+			return minMap.get(root);
+		}
+		int min=root.val;
+		if(root.left!=null){
+			min=Math.min(getMinRight(root.left), min);
+		}
+		if(root.right!=null){
+			min=Math.min(getMinRight(root.right), min);
+		}
+		minMap.put(root, min);
+		return min;
+	}
+	
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
