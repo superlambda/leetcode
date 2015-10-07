ec569dff12d430dc2d5c82a5bcf6a86abed7f07d
superlambda <yingning200507@163.com>
2015-10-07 10:38:32 +0800

    o(n) solution

--- src/peter/liu/longestconsecutivesequence/Solution.java
+++ src/peter/liu/longestconsecutivesequence/Solution.java
@@ -1,6 +1,6 @@
 package peter.liu.longestconsecutivesequence;
 
-import java.util.Arrays;
+import java.util.TreeSet;
 
 /**
  * Given an unsorted array of integers, find the length of the longest
@@ -15,17 +15,44 @@ import java.util.Arrays;
  *
  */
 public class Solution {
+//	public int longestConsecutive(int[] nums) {
+//		if(nums.length==1){
+//			return 1;
+//		}
+//		Arrays.sort(nums);
+//		int longest = 0;
+//		int candidate = 1;
+//		for (int i = 0; i < nums.length - 1; i++) {
+//			if (nums[i + 1] - nums[i] == 1) {
+//				candidate++;
+//			} else if(nums[i + 1] - nums[i] >1) {
+//				candidate = 1;
+//			}
+//			longest = candidate > longest ? candidate : longest;
+//		}
+//		return longest;
+//	}
+	
 	public int longestConsecutive(int[] nums) {
-		if(nums.length==1){
+		
+		TreeSet<Integer> set =new TreeSet<>();
+		for(int i=0;i<nums.length;i++){
+			set.add(nums[i]);
+		}
+		int[] newNums=new int[set.size()];
+		int index=0;
+		for(Integer number:set){
+			newNums[index++]=number;
+		}
+		if(newNums.length==1){
 			return 1;
 		}
-		Arrays.sort(nums);
 		int longest = 0;
 		int candidate = 1;
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i + 1] - nums[i] == 1) {
+		for (int i = 0; i < newNums.length - 1; i++) {
+			if (newNums[i + 1] - newNums[i] == 1) {
 				candidate++;
-			} else if(nums[i + 1] - nums[i] >1) {
+			} else if(newNums[i + 1] - newNums[i] >1) {
 				candidate = 1;
 			}
 			longest = candidate > longest ? candidate : longest;
