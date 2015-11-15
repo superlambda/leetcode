0f52ba7f4412b1afb83e6a77ffc5debae1dddefb
superlambda <yingning200507@163.com>
2015-11-15 16:22:16 +0800

    ac

---
+++ src/peter/liu/largestnumber/Solution.java
@@ -0,0 +1,41 @@
+package peter.liu.largestnumber;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Given a list of non negative integers, arrange them such that they form the
+ * largest number.
+ * 
+ * For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+ * 
+ * Note: The result may be very large, so you need to return a string instead of
+ * an integer.
+ * 
+ * @author liuyingjie
+ *
+ */
+public class Solution {
+	public String largestNumber(int[] nums) {
+		Integer[] ns = new Integer[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			ns[i] = nums[i];
+		}
+		Arrays.sort(ns, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer h1, Integer h2) {
+				String s1 = String.valueOf(h1);
+				String s2 = String.valueOf(h2);
+				return (s2+s1).compareTo(s1+s2);
+			}
+		});
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < ns.length; i++) {
+			sb.append(ns[i]);
+		}
+		if(sb.charAt(0)=='0'){
+			return "0";
+		}
+		return sb.toString();
+	}
+}
---
+++ src/peter/liu/largestnumber/SolutionTest.java
@@ -0,0 +1,15 @@
+package peter.liu.largestnumber;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testLargestNumber() {
+		System.out.println("121".compareTo("120"));
+		System.out.println('0');
+	}
+
+}
