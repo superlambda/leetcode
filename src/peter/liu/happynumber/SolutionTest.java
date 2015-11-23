c7c7bc8fed73ff6bdbc8c53405d4fd0d34249d00
superlambda <yingning200507@163.com>
2015-11-23 21:23:50 +0800

    ac

---
+++ src/peter/liu/happynumber/SolutionTest.java
@@ -0,0 +1,14 @@
+package peter.liu.happynumber;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testIsHappy() {
+		System.out.println(new Solution().isHappy(7));
+	}
+
+}
