b582e4094be736ee7bf7eacd39c193c7ddc8ccca
superlambda <yingning200507@163.com>
2015-11-23 21:11:14 +0800

    more efficient way from discussion.

--- src/peter/liu/numberofislands/Solution.java
+++ src/peter/liu/numberofislands/Solution.java
@@ -31,7 +31,7 @@ import java.util.Set;
 public class Solution {
 	private Map<String,Set<String>> islandMap=new HashMap<>();
 	private Map<String,String> candidateToIslandMap = new HashMap<>();
-	public int numIslands(char[][] grid) {
+	public int numIslandsByPeter(char[][] grid) {
 		for(int i=0;i<grid.length;i++){
 			for(int j=0;j<grid[0].length;j++){
 				if(grid[i][j]=='1'){
@@ -89,5 +89,32 @@ public class Solution {
 			}
 		}
 		return islandMap.keySet().size();
+	}
+	
+	public int numIslands(char[][] grid) {
+		int count = 0;
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] == '1') {
+					count++;
+					clearRestOfLand(grid, i, j);
+				}
+			}
+		}
+		return count;
+	}
+
+	private void clearRestOfLand(char[][] grid, int i, int j) {
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0'){
+			return;
+		}
+
+		grid[i][j] = '0';
+		clearRestOfLand(grid, i + 1, j);
+		clearRestOfLand(grid, i - 1, j);
+		clearRestOfLand(grid, i, j + 1);
+		clearRestOfLand(grid, i, j - 1);
+		return;
 	}
 }
