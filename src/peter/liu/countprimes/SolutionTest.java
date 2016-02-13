8466f6d3e6184c837ad7c601163920e2a4b66aeb
superlambda <yingning200507@163.com>
2016-02-13 21:24:20 +0800

    ac

---
+++ src/peter/liu/countprimes/SolutionTest.java
@@ -0,0 +1,12 @@
+package peter.liu.countprimes;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testCountPrimes() {
+		System.out.println(new Solution().countPrimes(1500000));
+	}
+
+}
