b75f42e7ece672d981d87b28791e908268f349b1
superlambda <yingning200507@163.com>
2016-04-01 21:26:17 +0800

    new effective version

--- src/peter/liu/substringwithconcatenationofallwords/Solution.java
+++ src/peter/liu/substringwithconcatenationofallwords/Solution.java
@@ -1,11 +1,9 @@
 package peter.liu.substringwithconcatenationofallwords;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * You are given a string, s, and a list of words, words, that are all of the
@@ -23,46 +21,96 @@ import java.util.Set;
  *
  */
 public class Solution {
+	// public List<Integer> findSubstring(String s, String[] words) {
+	// List<Integer> result = new ArrayList<>();
+	// if (words.length == 0) {
+	// return result;
+	// }
+	// int wordLength = words[0].length();
+	// int subLength = words.length * wordLength;
+	// Map<String, Integer> wordMap = new HashMap<>();
+	// for (String word : words) {
+	// if (!wordMap.containsKey(word)) {
+	// wordMap.put(word, 1);
+	// } else {
+	// wordMap.put(word, wordMap.get(word) + 1);
+	// }
+	// }
+	//
+	// for (int i = 0; i <= s.length() - subLength; i++) {
+	// int startIndex = i;
+	// if (wordMap.containsKey(s.substring(startIndex, startIndex +
+	// wordLength))) {
+	// Map<String, Integer> checkMap = new HashMap<>();
+	// while (startIndex < i + subLength) {
+	// String subString = s.substring(startIndex, startIndex + wordLength);
+	// if (wordMap.containsKey(subString)) {
+	// checkMap.put(subString, !checkMap.containsKey(subString) ? 1 :
+	// checkMap.get(subString) + 1);
+	// startIndex += wordLength;
+	// } else {
+	// break;
+	// }
+	// }
+	// if (checkMap.equals(wordMap)) {
+	// result.add(i);
+	// }
+	// }
+	//
+	// }
+	// return result;
+	// }
+
 	public List<Integer> findSubstring(String s, String[] words) {
-		List<Integer> result=new ArrayList<>();
-		if(words.length==0){
-			return result;
+		List<Integer> res = new LinkedList<>();
+		if (words.length == 0 || s.length() < words.length * words[0].length()){
+			return res;
 		}
-		int subLength=words.length*words[0].length();
-		int wordLength=words[0].length();
-		Map<String,Integer> wordMap=new HashMap<>();
-		for(String word:words){
-			if(!wordMap.containsKey(word)){
-				wordMap.put(word, 1);
-			}else{
-				wordMap.put(word, wordMap.get(word)+1);
-			}
+		int strLength = s.length(), arrayLength = words.length, wordLength = words[0].length();
+		Map<String, Integer> map = new HashMap<>(), curMap = new HashMap<>();
+		for (String word : words) {
+			if (map.containsKey(word))
+				map.put(word, map.get(word) + 1);
+			else
+				map.put(word, 1);
 		}
-		
-		for(int i=0;i<=s.length()-subLength;i++){
-			
-			Map<String,Integer> checkMap=new HashMap<>(wordMap);
-			int startIndex=i;
-			while(startIndex<=i+subLength){
-				String subString=s.substring(startIndex,startIndex+wordLength);
-				if(checkMap.containsKey(subString)){
-					int count=checkMap.get(subString);
-					if(count==1){
-						checkMap.remove(subString);
+		String str = null, tmp = null;
+		for (int i = 0; i < wordLength; i++) {
+			int count = 0; // remark: reset count
+			for (int left = i, right = i; right + wordLength <= strLength; right += wordLength) {
+				str = s.substring(right, right + wordLength);
+				if (map.containsKey(str)) {
+					if (curMap.containsKey(str)){
+						curMap.put(str, curMap.get(str) + 1);
 					}else{
-						checkMap.put(subString, count-1);
+						curMap.put(str, 1);
 					}
-					startIndex+=wordLength;
-					if(checkMap.isEmpty()){
-						result.add(i);
-						break;
+					if (curMap.get(str) <= map.get(str)){
+						count++;
 					}
-				}else{
-					break;
+					while (curMap.get(str) > map.get(str)) {
+						tmp = s.substring(left, left + wordLength);
+						curMap.put(tmp, curMap.get(tmp) - 1);
+						left += wordLength;
+						if (curMap.get(tmp) < map.get(tmp)){
+							count--;
+						}
+					}
+					if (count == arrayLength) {
+						res.add(left);
+						tmp = s.substring(left, left + wordLength);
+						curMap.put(tmp, curMap.get(tmp) - 1);
+						left += wordLength;
+						count--;
+					}
+				} else {
+					curMap.clear();
+					count = 0;
+					left = right + wordLength;
 				}
-				
 			}
+			curMap.clear();
 		}
-		return result;
+		return res;
 	}
 }
