99cab0391a4998dc9ca84c4080bd394c60e823b9
superlambda <yingning200507@163.com>
2017-07-05 20:58:42 +0800

    dynamic programming with stack implementation

--- src/peter/liu/maximalrectangle/Solution.java
+++ src/peter/liu/maximalrectangle/Solution.java
@@ -1,9 +1,7 @@
 package peter.liu.maximalrectangle;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 
@@ -21,88 +19,76 @@ import java.util.Map;
  */
 public class Solution {
 	
+	
 	public int maximalRectangle(char[][] matrix) {
 		
-		int maxRectangle = 0;
-		Map<String, List<int[]>> vertexMap = new HashMap<>(793);
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				if(i-2>=0&&matrix[i-2][j]=='1'){
-					vertexMap.remove((i - 2) + "," + j);
+		if (matrix.length== 0){
+			return 0;
+		}
+		int[] heightArray=new int[matrix[0].length];
+		int maxArea=0;
+		for(int i=0;i<matrix.length;i++){
+			for(int j=0;j<matrix[i].length;j++){
+				if(matrix[i][j]=='1'){
+					heightArray[j] += 1;
+				}else{
+					heightArray[j] = 0;
 				}
-				
-				if(i-1>=0&&j-1>=0&&matrix[i-1][j-1]=='1'){
-					vertexMap.remove((i - 1) + "," + (j-1));
-				}
-				
-				if (matrix[i][j] == '0') {
-					continue;
-				}
-				
-				String itself=i + "," + j;
-				List<int[]> list = new LinkedList<>();
-				int[] vertexitSelf ={i,j};
-				list.add(vertexitSelf);
-
-				List<int[]> vertPreList = null;
-				List<int[]> horizPreList = null;
-				if(i>=1){
-					vertPreList = vertexMap.get((i - 1) + "," + j);
-				}
-				if(j>=1){
-					horizPreList = vertexMap.get((i) + "," + (j - 1));
-				}
-				
-				if(vertPreList==null && horizPreList==null){
-					vertexMap.put(itself, list);
-					maxRectangle = maxRectangle < 1? 1:maxRectangle;
-					continue;
-				}
-				
-				if(vertPreList ==null){
-					vertPreList =  new LinkedList<>();
-				}
-				if(horizPreList ==null){
-					horizPreList =  new LinkedList<>();
-				}
-				
-				for(int[] vertex: vertPreList){	
-					if((!horizPreList.isEmpty())&&contains(horizPreList,vertex)){
-						list.add(vertex);
-						int area = (i - vertex[0] + 1)*(j - vertex[1] + 1);
-						maxRectangle = maxRectangle < area ? area : maxRectangle;
-					}else{
-						if(vertex[1]==j){
-							list.add(vertex);
-							int area = i - vertex[0] + 1;
-							maxRectangle = maxRectangle < area ? area : maxRectangle;
-						}
-					}
-				}
-			
-				for(int[] vertex: horizPreList){
-					if(vertPreList.isEmpty()||(!contains(vertPreList,vertex))){
-						if(vertex[0]==i){
-							list.add(vertex);
-							int area = j - vertex[1] + 1;
-							maxRectangle = maxRectangle < area ? area : maxRectangle;
-						}
-					}
-				}	
-				
-				vertexMap.put(itself, list);
+			}
+			int area=maxHistogram(heightArray);
+			if (area>maxArea){
+				maxArea=area;
 			}
 		}
-		return maxRectangle;
+		
+		return maxArea;
 	}
-	
-	private boolean contains(List<int[]> preList,int[] vertex){
-		for(int[] vertixToCheck: preList){
-			if(vertixToCheck[0]==vertex[0]&&vertixToCheck[1]==vertex[1]){
-				return true;
-			}
-		}
-		return false;
-	}
+	public int maxHistogram(int heights[]){
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int maxArea = 0;
+        int area = 0;
+        int i;
+        for(i=0; i < heights.length;){
+            if(stack.isEmpty() || heights[stack.peekFirst()] <= heights[i]){
+                stack.offerFirst(i++);
+            }else{
+                int top = stack.pollFirst();
+                //if stack is empty means everything till i has to be
+                //greater or equal to heights[top] so get area by
+                //heights[top] * i;
+                if(stack.isEmpty()){
+                    area = heights[top] * i;
+                }
+                //if stack is not empty then everythin from i-1 to heights.peek() + 1
+                //has to be greater or equal to heights[top]
+                //so area = heights[top]*(i - stack.peek() - 1);
+                else{
+                    area = heights[top] * (i - stack.peekFirst() - 1);
+                }
+                if(area > maxArea){
+                    maxArea = area;
+                }
+            }
+        }
+        while(!stack.isEmpty()){
+            int top = stack.pollFirst();
+            //if stack is empty means everything till i has to be
+            //greater or equal to heights[top] so get area by
+            //heights[top] * i;
+            if(stack.isEmpty()){
+                area = heights[top] * i;
+            }
+            //if stack is not empty then everything from i-1 to heights.peek() + 1
+            //has to be greater or equal to heights[top]
+            //so area = heights[top]*(i - stack.peek() - 1);
+            else{
+                area = heights[top] * (i - stack.peekFirst() - 1);
+            }
+        if(area > maxArea){
+                maxArea = area;
+            }
+        }
+        return maxArea;
+    }
 
 }
--- src/peter/liu/maximalrectangle/SolutionTest.java
+++ src/peter/liu/maximalrectangle/SolutionTest.java
@@ -1,8 +1,9 @@
 package peter.liu.maximalrectangle;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
 
 public class SolutionTest {
 
@@ -14,6 +15,17 @@ public class SolutionTest {
 						    { '1', '0', '0', '1', '0' } };
 		
 		assertEquals(new Solution().maximalRectangle(matrix),6);
+		
+//		String ttt="1,2";
+//		System.out.println(ttt.substring(2,3));
+//		Pair i=new Pair(1,2);
+//		Pair j=new Pair(1,2);;
+//		Set<Pair> set= new HashSet<>();
+//		set.add(i);
+//		set.add(j);
+//		System.out.println(set.size());
+		
 	}
+	
 
 }
